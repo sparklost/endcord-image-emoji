2a36ef594136ecc079dc2a2637a1434b5db00596
sparklost <marko.b.zivic@gmail.com>
2026-05-10 18:49:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# endcord-image-emoji
+An extension for [endcord](https://github.com/sparklost/endcord) discord TUI client, that adds drawing custom discord emoji using kitty protocol.  
+Should work on all terminals with kitty image protocol support.  
+
+## Installing
+See [official extensions documentation](https://github.com/sparklost/endcord/blob/main/extensions.md#installing-extensions) for installing instructions.
+Available options:
+- Git clone into `Extensions` directory located in endcord config directory.
+- Run `endcord -i https://github.com/sparklost/endcord-image-emoji`
+- Or use endcord client-side command `install_extension sparklost/endcord-image-emoji`
+
+
+## Disclaimer
+This extension is usable only in bots which should not be breaking any ToS, byt here's a warning anyway:  
+> [!WARNING]
+> Using third-party client is against Discord's Terms of Service and may cause your account to be banned!  
+> **Use endcord and/or this extension at your own risk!**  
+> If this extension is modified, it may be used for harmful or unintended purposes.  
+> **The developer is not responsible for any misuse or for actions taken by users.**  
---
+++ endcord-image-emoji.py
@@ -0,0 +1,195 @@
+import base64
+import logging
+import os
+import sys
+import threading
+
+from endcord import peripherals
+
+EXT_NAME = "Image Emoji"
+EXT_VERSION = "0.1.0"
+EXT_ENDCORD_VERSION = "1.5.0"
+EXT_DESCRIPTION = "An extension that adds drawing custom discord emoji using kitty protocol"
+EXT_SOURCE = "https://github.com/sparklost/endcord-image-emoji"
+logger = logging.getLogger(__name__)
+
+START_IMAGE_ID = 4000
+
+
+def check_kitty():
+    """Check if kitty protocol is supported"""
+    if sys.platform == "win32":
+        return False
+    import termios
+    import tty
+    stdin_fd = sys.stdin.fileno()
+    old_term = termios.tcgetattr(stdin_fd)
+    try:
+        tty.setraw(stdin_fd)
+        os.write(stdin_fd, b"\x1b_Gi=1,s=1,v=1,a=q,t=d,f=24;AAAA\x1b\\\x1b[c")
+        response = os.read(stdin_fd, 1024)
+        return b"OK" in response
+    finally:
+        termios.tcsetattr(stdin_fd, termios.TCSADRAIN, old_term)
+
+
+def kitty_upload_png(path, image_id):
+    """Upload base64 encoded png into kitty image cache"""
+    with open(path, "rb") as f:
+        png_data = f.read()
+    payload = base64.b64encode(png_data).decode("ascii")
+    for i in range(0, len(payload), 4096):
+        chunk = payload[i:i + 4096]
+        more = 1 if i + 4096 < len(payload) else 0
+        if i == 0:
+            header = f"a=t,f=100,q=2,i={image_id},m={more}"
+        else:
+            header = f"m={more}"
+        os.write(sys.stdout.fileno(), f"\033_G{header};{chunk}\033\\".encode())
+
+
+def kitty_draw_image_by_id(image_id, x, y, w=None, h=None):
+    """Draw previously uploaded image by its id"""
+    os.write(sys.stdout.fileno(), f"\0337\033[{y+1};{x+1}H".encode())   # save and move cursor
+    header = f"a=p,q=2,z=-1,i={image_id}"
+    if w is not None:
+        header += f",c={w}"
+    if h is not None:
+        header += f",r={h}"
+    os.write(sys.stdout.fileno(), f"\033_G{header}\033\\\0338".encode())   # \0338 is restore cursor
+
+
+def kitty_delete_images_by_id(image_id):
+    """Delete all images with this id and remove it from memory"""
+    os.write(sys.stdout.fileno(), f"\033_Ga=d,d=I,q=2,i={image_id}\033\\".encode())
+
+
+def kitty_clear_images_by_id(image_id):
+    """Delete all images with this id but keep image in memory"""
+    os.write(sys.stdout.fileno(), f"\033_Ga=d,d=i,q=2,i={image_id}\033\\".encode())
+
+
+class Extension:
+    """Main extension class"""
+
+    def __init__(self, app):
+        self.app = app
+        kitty_supported = getattr(self.app.tui, "kitty_supported", None)
+        if kitty_supported is False or (kitty_supported is not True and not check_kitty()):
+            logger.warning("No kitty protocol support detected in this terminal")
+            self.run = False
+            del type(self).on_chat_update
+            del type(self).on_chat_draw
+            self.app.tui.kitty_supported = False
+            return
+
+        self.app.placeholder_emoji = True
+        self.app.formatter.placeholder_emoji = "  "
+
+        self.run = True
+        self.chat_map = []
+        self.update = threading.Event()
+        self.drawing = threading.Event()
+        self.image_ids = {}
+        self.emoji_pos_cache = []
+        self.prev_chat_index = None
+        self.prev_chat_hw = None
+        self.force_draw = False
+        self.image_ids_lock = threading.Lock()
+
+        threading.Thread(target=self.worker, daemon=True).start()
+
+
+    def on_chat_update(self, chat, chat_format, chat_map):   # noqa
+        """Get new chat map"""
+        self.chat_map = chat_map
+        self.update.set()
+
+
+    def on_chat_draw(self):
+        """Re-calculate image positions and draw them"""
+        _, chat_x = self.app.tui.win_chat.getbegyx()
+        h = self.app.tui.chat_hw[0]
+        if not self.force_draw and self.prev_chat_index == self.app.tui.chat_index and self.prev_chat_hw == self.app.tui.chat_hw:
+            return
+        with self.app.tui.lock:
+            with self.image_ids_lock:
+                for kitty_image_id in self.image_ids.values():
+                    kitty_clear_images_by_id(kitty_image_id)
+            for rel_y, rel_x, kitty_image_id in self.emoji_pos_cache:
+                abs_y = h - (rel_y - self.app.tui.chat_index - self.app.tui.have_title + 1)
+                if abs_y <= 0 or abs_y > h:
+                    continue
+                abs_x = chat_x + rel_x
+                kitty_draw_image_by_id(kitty_image_id, x=abs_x, y=abs_y, w=None, h=1)
+        self.prev_chat_index = self.app.tui.chat_index
+        self.prev_chat_hw = self.app.tui.chat_hw
+
+
+    def get_free_id(self):
+        """Get first free id"""
+        for i in range(len(self.image_ids.values()) - 1):
+            if self.image_ids.values()[i + 1] != self.image_ids.values()[i] + 1:
+                return self.image_ids.values()[i] + 1
+        return START_IMAGE_ID
+
+
+    def worker(self):
+        """Thread that updates emoji cache on disk and in ram and downloads missing emoji"""
+        while self.run:
+            self.update.wait()
+            self.update.clear()
+            visible = []
+            new_emoji_pos_cache = []
+            new_image_ids = []
+            self.force_draw = False
+
+            for rel_y, line_map in enumerate(self.chat_map):
+                if not line_map:
+                    continue
+                if line_map[3]:
+                    iterable = line_map[3]
+                elif line_map[5] and line_map[5][2]:
+                    iterable = line_map[5][2]
+                else:
+                    iterable = ()
+
+                for emoji in iterable:
+                    if line_map[3]:
+                        _, rel_x, emoji_id = emoji
+                        rel_x -= 1
+                    else:
+                        rel_x, _, emoji_id = emoji
+                    if not emoji_id:
+                        continue
+
+                    if emoji_id in self.image_ids:
+                        kitty_image_id = self.image_ids[emoji_id]
+                    else:
+                        image_path = self.app.discord.get_emoji(emoji_id, size=None, img_type="png", cache=os.path.join(peripherals.cache_path, "emoji"))
+                        kitty_image_id = self.get_free_id()
+                        kitty_upload_png(image_path, kitty_image_id)
+                        new_image_ids.append((emoji_id, kitty_image_id))
+                        self.force_draw = True
+
+                    visible.append(emoji_id)
+                    new_emoji_pos_cache.append((rel_y, rel_x, kitty_image_id))
+
+            if new_emoji_pos_cache != self.emoji_pos_cache or self.force_draw:
+                self.emoji_pos_cache = new_emoji_pos_cache
+                logger.info(self.emoji_pos_cache)
+                self.force_draw = True
+                self.on_chat_draw()
+
+            # delete caches
+            with self.image_ids_lock:
+                for emoji_id, kitty_image_id in new_image_ids:
+                    self.image_ids[emoji_id] = kitty_image_id
+                to_delete = [k for k in self.image_ids if k not in visible]
+                deleted_kitty = []
+                for emoji_id in to_delete:
+                    deleted_kitty.append(self.image_ids.pop(emoji_id))
+            for kitty_image_id in deleted_kitty:
+                if kitty_image_id not in self.image_ids.values():
+                    with self.app.tui.lock:
+                        kitty_delete_images_by_id(kitty_image_id)
